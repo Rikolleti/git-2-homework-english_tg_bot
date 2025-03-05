4381e5f317f6b73fc7c8b7871bcdb4e3aa46921c
azmamedov <azmamedov@ozon.ru>
2025-03-05 21:15:09 +0300

    Reformatted with black

--- db.py
+++ db.py
@@ -1,19 +1,25 @@
 import psycopg2
 
+
 def get_connection():
     return psycopg2.connect(database="english_words", user="postgres", password="postgres")
+
 
 def drop_table():
     with get_connection() as conn:
         with conn.cursor() as cur:
-            cur.execute("""
+            cur.execute(
+                """
             DROP TABLE IF EXISTS word_list;
-            """)
+            """
+            )
+
 
 def create_table():
     with get_connection() as conn:
         with conn.cursor() as cur:
-            cur.execute("""
+            cur.execute(
+                """
             CREATE TABLE IF NOT EXISTS word_list(
                 word_id SERIAL PRIMARY KEY,
                 target_word VARCHAR(60) NOT NULL,
@@ -21,81 +27,98 @@ def create_table():
                 other_words VARCHAR(255),
                 user_id BIGINT
             );
-            """)
+            """
+            )
+
 
 def insert_words(target_word: str, russian_word: str, other_words: str):
     with get_connection() as conn:
         with conn.cursor() as cur:
-            cur.execute("""
+            cur.execute(
+                """
             INSERT INTO word_list (target_word, russian_word, other_words)
             VALUES (%s, %s, %s)
             RETURNING word_id;
-            """, (target_word, russian_word, other_words))
+            """,
+                (target_word, russian_word, other_words),
+            )
             show_word_id = cur.fetchone()[0]
             return show_word_id
+
 
 def insert_words_from_user(target_word: str, russian_word: str, other_words: str, user_id: int):
     with get_connection() as conn:
         with conn.cursor() as cur:
-            cur.execute("""
+            cur.execute(
+                """
             INSERT INTO word_list (target_word, russian_word, other_words, user_id)
             VALUES (%s, %s, %s, %s)
             RETURNING word_id;
-            """, (target_word, russian_word, other_words, user_id))
+            """,
+                (target_word, russian_word, other_words, user_id),
+            )
             show_word_id = cur.fetchone()[0]
             return show_word_id
+
 
 def show_words(user_id):
     with get_connection() as conn:
         with conn.cursor() as cur:
-            cur.execute("""
+            cur.execute(
+                """
                 SELECT word_id, target_word, russian_word, other_words
                 FROM word_list
                 WHERE user_id IS NULL OR user_id = %s;
-            """, (user_id,))
+            """,
+                (user_id,),
+            )
             rows = cur.fetchall()
             words = [
-                {
-                    'word_id': row[0],
-                    'target_word': row[1],
-                    'russian_word': row[2],
-                    'other_words': row[3]
-                }
+                {"word_id": row[0], "target_word": row[1], "russian_word": row[2], "other_words": row[3]}
                 for row in rows
             ]
             return words
 
+
 def count_words(user_id: int):
     with get_connection() as conn:
         with conn.cursor() as cur:
-            cur.execute("""
+            cur.execute(
+                """
             SELECT COUNT(target_word) FROM word_list
             WHERE user_id IS NULL OR user_id = %s;
-            """, (user_id,))
+            """,
+                (user_id,),
+            )
             result = cur.fetchone()[0]
             return result
+
 
 def delete_word(russian_word: str, user_id: int):
     with get_connection() as conn:
         with conn.cursor() as cur:
-            output = cur.execute("""
+            output = cur.execute(
+                """
             DELETE FROM word_list
             WHERE russian_word = %s AND user_id = %s;
-            """, (russian_word, user_id))
+            """,
+                (russian_word, user_id),
+            )
             if output is not None:
                 print(f"Word {russian_word} deleted for user {user_id}")
         return cur.rowcount > 0
 
+
 # Инициализация таблицы
 drop_table()
 create_table()
-insert_words('Peace', 'Мир', 'King, Break, Stop')
-insert_words('Friend', 'Друг', 'Topic, Look, Drop')
-insert_words('Love', 'Любовь', 'Heart, Feel, Passion')
-insert_words('Sun', 'Солнце', 'Light, Day, Sky')
-insert_words('Water', 'Вода', 'Ocean, River, Drink')
-insert_words('Tree', 'Дерево', 'Forest, Leaf, Nature')
-insert_words('Book', 'Книга', 'Read, Page, Story')
-insert_words('Music', 'Музыка', 'Sound, Song, Melody')
-insert_words('Time', 'Время', 'Clock, Moment, Hour')
-insert_words('House', 'Дом', 'Apartment, Room, Building')
+insert_words("Peace", "Мир", "King, Break, Stop")
+insert_words("Friend", "Друг", "Topic, Look, Drop")
+insert_words("Love", "Любовь", "Heart, Feel, Passion")
+insert_words("Sun", "Солнце", "Light, Day, Sky")
+insert_words("Water", "Вода", "Ocean, River, Drink")
+insert_words("Tree", "Дерево", "Forest, Leaf, Nature")
+insert_words("Book", "Книга", "Read, Page, Story")
+insert_words("Music", "Музыка", "Sound, Song, Melody")
+insert_words("Time", "Время", "Clock, Moment, Hour")
+insert_words("House", "Дом", "Apartment, Room, Building")
--- main.py
+++ main.py
@@ -9,39 +9,46 @@ from telebot.storage import StateMemoryStorage
 state_storage = StateMemoryStorage()
 
 config = configparser.ConfigParser()
-config.read('settings.ini')
-token_tg = config['Telegram']['token_tg'].strip()
+config.read("settings.ini")
+token_tg = config["Telegram"]["token_tg"].strip()
 
 TOKEN = token_tg
 bot = telebot.TeleBot(TOKEN, state_storage=state_storage)
 
+
 class Command:
-    START = 'Начать 🚀'
-    ADD_WORD = 'Добавить слово ➕'
-    DELETE_WORD = 'Удалить слово🔙'
-    NEXT = 'Дальше ⏭'
-    HELP = 'Помощь️ ℹ️'
+    START = "Начать 🚀"
+    ADD_WORD = "Добавить слово ➕"
+    DELETE_WORD = "Удалить слово🔙"
+    NEXT = "Дальше ⏭"
+    HELP = "Помощь️ ℹ️"
+
 
 class MyStates(StatesGroup):
     target_word = State()
     russian_word = State()
     other_words = State()
 
+
 def from_db(user_id):
     words = db.show_words(user_id)
     return words
+
 
 def count(user_id):
     count_words = db.count_words(user_id)
     return count_words
 
+
 def delete_word_from_db(russian_word, user_id):
     res = db.delete_word(russian_word, user_id)
     return res
 
-def insert_words(target_word,russian_word,other_word,user_id):
-    res = db.insert_words_from_user(target_word,russian_word,other_word,user_id)
+
+def insert_words(target_word, russian_word, other_word, user_id):
+    res = db.insert_words_from_user(target_word, russian_word, other_word, user_id)
     return res
+
 
 # HELP
 @bot.message_handler(func=lambda message: message.text == Command.HELP)
@@ -58,8 +65,9 @@ def help_command(message):
     """
     bot.send_message(message.chat.id, help_text, parse_mode="Markdown")
 
+
 # ВЫВОД СЛОВ
-@bot.message_handler(commands=['cards', 'start'])
+@bot.message_handler(commands=["cards", "start"])
 def create_cards(message):
     user = message.from_user
     greeting = (
@@ -76,17 +84,12 @@ def create_cards(message):
     markup.add(start_btn)
     bot.send_message(message.chat.id, greeting, reply_markup=markup)
 
+
 @bot.message_handler(func=lambda message: message.text == Command.START)
 def start(message):
     user = message.from_user
     show_card(message, user.id, 0)
 
-@bot.message_handler(func=lambda message: message.text == Command.NEXT)
-def next_cards(message):
-    user = message.from_user
-    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
-        current_index = data.get('current_index', 0) + 1
-    show_card(message, user.id, current_index)
 
 def show_card(message, user_id, current_index):
     words = from_db(user_id)
@@ -95,9 +98,9 @@ def show_card(message, user_id, current_index):
         return
     if current_index < len(words):
         word = words[current_index]
-        target_word = word['target_word']
-        russian_word = word['russian_word']
-        other_words = word['other_words']
+        target_word = word["target_word"]
+        russian_word = word["russian_word"]
+        other_words = word["other_words"]
 
         markup = types.ReplyKeyboardMarkup(row_width=2)
         target_word_btns = [types.KeyboardButton(target_word)]
@@ -118,11 +121,20 @@ def show_card(message, user_id, current_index):
         bot.send_message(message.chat.id, choose, reply_markup=markup)
         bot.set_state(message.from_user.id, MyStates.target_word, message.chat.id)
         with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
-            data['target_word'] = target_word
-            data['russian_word'] = russian_word
-            data['current_index'] = current_index
+            data["target_word"] = target_word
+            data["russian_word"] = russian_word
+            data["current_index"] = current_index
     else:
         bot.send_message(message.chat.id, "Ты выучил все слова !", reply_markup=telebot.types.ReplyKeyboardRemove())
+
+
+@bot.message_handler(func=lambda message: message.text == Command.NEXT)
+def next_cards(message):
+    user = message.from_user
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        current_index = data.get("current_index", 0) + 1
+    show_card(message, user.id, current_index)
+
 
 # ДОБАВЛЕНИЕ СЛОВ
 @bot.message_handler(func=lambda message: message.text == Command.ADD_WORD)
@@ -130,15 +142,18 @@ def add_word(message):
     bot.send_message(message.chat.id, "Введите слово на английском:")
     bot.register_next_step_handler(message, process_target_word)
 
+
 def process_target_word(message):
     target_word = message.text
     bot.send_message(message.chat.id, "Введите перевод на русский:")
     bot.register_next_step_handler(message, process_russian_word, target_word)
 
+
 def process_russian_word(message, target_word):
     russian_word = message.text
     bot.send_message(message.chat.id, "Введите 3 некорретных варианта перевода на английском через запятую:")
     bot.register_next_step_handler(message, process_other_words, target_word, russian_word)
+
 
 def process_other_words(message, target_word, russian_word):
     other_words = message.text
@@ -156,6 +171,7 @@ def delete_word(message):
     bot.send_message(message.chat.id, "Введите слово на русском, которое хотите удалить:")
     bot.register_next_step_handler(message, process_delete_word)
 
+
 def process_delete_word(message):
     russian_word = message.text
     user_id = message.from_user.id
@@ -168,32 +184,34 @@ def process_delete_word(message):
         bot.send_message(message.chat.id, failed_output)
     from_db(user_id)
 
+
 # ПРОВЕРКА НА КОРРЕКТНОСТЬ ОТВЕТА
 @bot.message_handler(func=lambda message: True)
 def message_reply(message):
     success_messages = [
-        'Отлично, ты молодец! 😍',
-        'Так держать! 😍',
-        'Супер! Ты справляешься! 🎉',
-        'Великолепно! Продолжай в том же духе! 💪'
+        "Отлично, ты молодец! 😍",
+        "Так держать! 😍",
+        "Супер! Ты справляешься! 🎉",
+        "Великолепно! Продолжай в том же духе! 💪",
     ]
 
     failed_messages = [
-        'Ошибка, попробуй еще раз. 😊',
-        'Не переживай, ты почти у цели! 💪',
-        'Попробуй еще раз, у тебя обязательно получится! 😉',
-        'Ошибка — это часть обучения, попробуй снова! 🌟'
+        "Ошибка, попробуй еще раз. 😊",
+        "Не переживай, ты почти у цели! 💪",
+        "Попробуй еще раз, у тебя обязательно получится! 😉",
+        "Ошибка — это часть обучения, попробуй снова! 🌟",
     ]
 
     with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
-        target_word = data.get('target_word')
+        target_word = data.get("target_word")
     if message.text == target_word:
         bot.send_message(message.chat.id, random.choice(success_messages))
         next_cards(message)
     else:
         bot.send_message(message.chat.id, random.choice(failed_messages))
 
+
 ################MAIN################
-if __name__ == '__main__':
+if __name__ == "__main__":
     print("Bot is running")
     bot.polling(none_stop=True)
